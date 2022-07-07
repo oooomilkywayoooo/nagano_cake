8e1f17861db5ce425f78d819ad2467143d6d0301
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-07-07 23:33:54 +0000

    [Add]devise,refile,customer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "4570247dc2e641d6a189312fc68ea798.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
