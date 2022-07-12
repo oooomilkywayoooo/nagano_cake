41c50abc170e417b93939d6b72258d148bd358d1
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-07-12 14:13:45 +0000

    [Finish]商品新規登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,9 +4,13 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @genres = Genre.all
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
   end
 
   def show
