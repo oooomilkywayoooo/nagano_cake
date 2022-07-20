65f0361b60a4cbebb01ce3ce3a5778bb1f203d62
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-07-20 14:57:34 +0000

    [Add]トップページの作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,23 @@
 class Admin::OrdersController < ApplicationController
+  def show
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+  end
+
+  def update
+	  @order = Order.find(params[:id])
+	  if @order.update(order_params)
+	    flash[:success] = "注文ステータスを変更しました"
+	    redirect_to admin_order_path(@order)
+	  else
+	    render "show"
+	  end
+  end
+
+	private
+
+	def order_params
+		  params.require(:order).permit(:order_status)
+	end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,16 @@ class Order < ApplicationRecord
 
   enum payment_method: {"クレジットカード": 0, "銀行振込": 1}
   enum order_status: {"入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4}
+
+  def sum_of_price
+    item.add_tax_price * amount
+  end
+  
+  def shipping_fee
+    800
+  end
+  
+  def total_amount_billed
+    @total + shipping_fee
+  end
 end
