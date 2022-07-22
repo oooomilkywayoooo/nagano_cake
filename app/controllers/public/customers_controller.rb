73b23b1b74f59bc6f4c5868dba30f47e6b1036e5
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-07-22 00:30:16 +0000

    [Add]登録情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,11 +8,24 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "変更しました"
+      redirect_to public_customer_path(@customer)
+    else
+      render :edit
+    end
   end
 
   def quit
   end
 
   def out
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,5 @@
 class Public::ItemsController < ApplicationController
+  def index
+    @customer = current_customer
+  end
 end
