97704d5de86f7202b359455b20da052827a16830
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-07-29 23:39:25 +0000

    [Add]注文情報入力画面の不具合修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,22 +6,27 @@ class Public::OrdersController < ApplicationController
     @address = current_customer.postal_code + '　' + current_customer.address
   end
 
+  def confirm
+    binding.pry
+    @order = Order.new(order_params)
+  end
+
   def create
     @customer = current_customer
     @cart_items = current_customer.cart_items.all
-    @order = current_customer.orders.new(order_params)
   end
 
   def show
   end
 
   def info
+
   end
 
   def thanks
   end
 
   def order_params
-    params.require(:order).permit(:ship_address, :payment_method, :address_id, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :select_address, :address_id, :postal_code, :address, :name)
   end
 end
